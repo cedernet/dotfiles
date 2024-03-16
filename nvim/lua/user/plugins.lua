f310c76f2798bbd916b6a97bc6cf548578ce3fb1
Eric Cederberg <somecoderdude@gmail.com>
2024-03-16 16:17:28 +0200

    Reindent a bunch of files

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -11,96 +11,99 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	-- Monokai-pro theme
+require("lazy").setup(
 	{
-		'loctvl842/monokai-pro.nvim',
-		priority = 1000,
-		config = function()
-			require("monokai-pro").setup()
-			vim.cmd('colorscheme monokai-pro-spectrum')
-		end
-	},
-	-- {
-	-- 	'ribru17/bamboo.nvim',
-	-- 	lazy = false,
-	-- 	priority = 900,
-	-- 	config = function()
-	-- 		require('bamboo').setup {
-	-- 			-- optional configuration here
-	-- 		}
-	-- 		require('bamboo').load()
-	-- 	end,
-	-- },
-	-- Commenting support.
-	{ import = 'user.plugins.vim-commentary' },
-	-- Add, change, and delete surrounding text.
-	{ 'tpope/vim-surround' },
-	-- Allow plugins to enable repeating of commands.
-	{ 'tpope/vim-repeat' },
-	-- Jump to the last location when opening a file.
-	{ 'farmergreg/vim-lastplace' },
-	-- Automatically set the working directory to the project root.
-	{ import = 'user.plugins.vim-rooter' },
-	-- Automatically add closing brackets, quotes, etc.
-	{ 'windwp/nvim-autopairs', config = true },
-	-- Add smooth scrolling to avoid jarring jumps
-	{ 'karb94/neoscroll.nvim', config = true },
-	-- All closing buffers without closing the split window.
-	{ import = 'user.plugins.bufdelete' },
-	-- Split arrays and methods onto multiple lines, or join them back up.
-	{ import = 'user.plugins.treesj' },
-	-- Improved syntax highlighting
-	{ import = 'user.plugins.treesitter' },
-	-- Disable backgrounds to make vim transparent
-	-- { 'xiyaowong/transparent.nvim' },
-	-- Fuzzy finder
-	{ import = 'user.plugins.telescope' },
-	-- File tree
-	{ import = 'user.plugins.neo-tree' },
-	-- A Status line.
-	{ import = 'user.plugins.lualine' },
-	-- Display buffers as tabs.
-	{ import = 'user.plugins.bufferline' },
-	-- Display indentation lines.
-	{ import = 'user.plugins.indent-blankline' },
-	-- Add a dashboard.
-	{ import = 'user.plugins.dashboard-nvim' },
-	-- Git integration.
-	{ import = 'user.plugins.gitsigns' },
-	-- Git commands.
-	{ 'tpope/vim-fugitive', dependencies = 'tpope/vim-rhubarb' },
-	-- Floating terminal.
-	{ import = 'user.plugins.floaterm' },
-	-- Completion
-	{ import = 'user.plugins.cmp' },
-	-- Language Server Protocol.
-	{ import = 'user.plugins.lspconfig' },
-	-- PHP Refactoring Tools
-	{ import = 'user.plugins.phpactor' },
-	-- Harpoon file jumper
-	{ import = 'user.plugins.harpoon' },
-	-- Show overview of code on page
-	{ import = 'user.plugins.aerial' },
-	-- Sublime style multicursor
-	{ import = 'user.plugins.multicursors' },
-	-- PHP docblocks
-	{ 'brett-griffin/phpdocblocks.vim' },
-	-- Better comments
-	{ import = 'user.plugins.todo-comments' },
-	-- Easier to use marks/bookmarks
-	{ import = 'user.plugins.marks' },
-}, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
-	install = {
-		colorscheme = {
-			"monokai-pro-spectrum",
+		-- Monokai-pro theme
+		{
+			'loctvl842/monokai-pro.nvim',
+			priority = 1000,
+			config = function()
+				require("monokai-pro").setup()
+				vim.cmd('colorscheme monokai-pro-spectrum')
+			end
 		},
+		-- {
+		-- 	'ribru17/bamboo.nvim',
+		-- 	lazy = false,
+		-- 	priority = 900,
+		-- 	config = function()
+		-- 		require('bamboo').setup {
+		-- 			-- optional configuration here
+		-- 		}
+		-- 		require('bamboo').load()
+		-- 	end,
+		-- },
+		-- Commenting support.
+		{ import = 'user.plugins.vim-commentary' },
+		-- Add, change, and delete surrounding text.
+		{ 'tpope/vim-surround' },
+		-- Allow plugins to enable repeating of commands.
+		{ 'tpope/vim-repeat' },
+		-- Jump to the last location when opening a file.
+		{ 'farmergreg/vim-lastplace' },
+		-- Automatically set the working directory to the project root.
+		{ import = 'user.plugins.vim-rooter' },
+		-- Automatically add closing brackets, quotes, etc.
+		{ 'windwp/nvim-autopairs', config = true },
+		-- Add smooth scrolling to avoid jarring jumps
+		{ 'karb94/neoscroll.nvim', config = true },
+		-- All closing buffers without closing the split window.
+		{ import = 'user.plugins.bufdelete' },
+		-- Split arrays and methods onto multiple lines, or join them back up.
+		{ import = 'user.plugins.treesj' },
+		-- Improved syntax highlighting
+		{ import = 'user.plugins.treesitter' },
+		-- Disable backgrounds to make vim transparent
+		-- { 'xiyaowong/transparent.nvim' },
+		-- Fuzzy finder
+		{ import = 'user.plugins.telescope' },
+		-- File tree
+		{ import = 'user.plugins.neo-tree' },
+		-- A Status line.
+		{ import = 'user.plugins.lualine' },
+		-- Display buffers as tabs.
+		{ import = 'user.plugins.bufferline' },
+		-- Display indentation lines.
+		{ import = 'user.plugins.indent-blankline' },
+		-- Add a dashboard.
+		{ import = 'user.plugins.dashboard-nvim' },
+		-- Git integration.
+		{ import = 'user.plugins.gitsigns' },
+		-- Git commands.
+		{ 'tpope/vim-fugitive', dependencies = 'tpope/vim-rhubarb' },
+		-- Floating terminal.
+		{ import = 'user.plugins.floaterm' },
+		-- Completion
+		{ import = 'user.plugins.cmp' },
+		-- Language Server Protocol.
+		{ import = 'user.plugins.lspconfig' },
+		-- PHP Refactoring Tools
+		{ import = 'user.plugins.phpactor' },
+		-- Harpoon file jumper
+		{ import = 'user.plugins.harpoon' },
+		-- Show overview of code on page
+		{ import = 'user.plugins.aerial' },
+		-- Sublime style multicursor
+		{ import = 'user.plugins.multicursors' },
+		-- PHP docblocks
+		{ 'brett-griffin/phpdocblocks.vim' },
+		-- Better comments
+		{ import = 'user.plugins.todo-comments' },
+		-- Easier to use marks/bookmarks
+		{ import = 'user.plugins.marks' },
 	},
-})
+	{
+		checker = {
+			enabled = true,
+			notify = false,
+		},
+		change_detection = {
+			notify = false,
+		},
+		install = {
+			colorscheme = {
+				"monokai-pro-spectrum",
+			},
+		},
+	}
+)
--- nvim/lua/user/plugins/treesj.lua
+++ nvim/lua/user/plugins/treesj.lua
@@ -1,11 +1,11 @@
 return {
-  'Wansmer/treesj',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  keys = {
-    { 'JJ', function() require('treesj').join() end },
-    { 'SS', function() require('treesj').split() end },
-  },
-  opts = {
-    use_default_keymaps = false,
-  },
+	'Wansmer/treesj',
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	keys = {
+		{ 'JJ', function() require('treesj').join() end },
+		{ 'SS', function() require('treesj').split() end },
+	},
+	opts = {
+		use_default_keymaps = false,
+	},
 }
