8112c222dd3661cd0673bd045a5db6d86a2ac1b0
Eric Cederberg <somecoderdude@gmail.com>
2024-03-20 13:17:15 +0200

    Add Leenalle user command

---
+++ nvim/lua/user/commands.lua
@@ -0,0 +1,12 @@
+-- Parse Leena's lists
+vim.api.nvim_create_user_command(
+	"Leenalle",
+	function ()
+		vim.cmd("silent %s/\\d)/\r- /g")
+		vim.cmd("silent %s/-  /- /g")
+		vim.cmd("silent %s/\\s\\+$//ge")
+		vim.cmd("normal! ggdd")
+	end,
+	{}
+)
+
