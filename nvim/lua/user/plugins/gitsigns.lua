ecaab3cf3a4fa2c4e4ead9c724fc4d8abfce02e4
EbbeC <eric@cedernet.fi>
2024-03-28 01:42:32 +0200

    More small tweaks

--- nvim/lua/user/plugins/gitsigns.lua
+++ nvim/lua/user/plugins/gitsigns.lua
@@ -3,8 +3,8 @@ return {
 	'lewis6991/gitsigns.nvim',
 	lazy = false,
 	keys = {
-		{ 'h>', ':Gitsigns next_hunk<CR>'},
-		{ 'h<', ':Gitsigns prev_hunk<CR>'},
+		{ '>h', ':Gitsigns next_hunk<CR>'},
+		{ '<h', ':Gitsigns prev_hunk<CR>'},
 		{ 'gs', ':Gitsigns stage_hunk<CR>'},
 		{ 'gS', ':Gitsigns undo_stage_hunk<CR>'},
 		{ 'gp', ':Gitsigns preview_hunk<CR>'},
