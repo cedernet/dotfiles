412a073a8a93e82a14e0e38c0204aa056bcb0815
Eric Cederberg <somecoderdude@gmail.com>
2025-09-07 14:47:52 +0300

    Lazyload neogen / Add WTF

--- nvim/lua/user/plugins/neogen.lua
+++ nvim/lua/user/plugins/neogen.lua
@@ -1,11 +1,21 @@
 -- https://github.com/danymat/neogen
 return {
 	"danymat/neogen",
+	lazy = true,
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
-		require("neogen").setup({ snippet_engine = "luasnip", languages = { php = { template = { annotation_convention = "phpdoc" } } } })
-		-- Keymap: generate docblock for function/class at cursor
-		vim.keymap.set("n", "<leader>pd", function() require("neogen").generate() end,
-			{ desc = "Generate PHPDoc (Neogen)" })
+		require("neogen").setup({
+			snippet_engine = "luasnip",
+			languages = {
+				php = {
+					template = {
+						annotation_convention = "phpdoc",
+					},
+				}
+			},
+		})
 	end,
+	keys = {
+		{ "<leader>pd", "<cmd>Neogen<cr>", desc = "Neogen generate docblock" },
+	}
 }
